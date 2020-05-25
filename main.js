a69c842dc744bc2a2d5eeff60e1d51ff2fe47835
Oliver Ni <oliver.ni@gmail.com>
2020-05-25 12:38:53 -0700

    Change model

--- main.js
+++ main.js
@@ -5,15 +5,16 @@ const app = new Vue({
         mean: null,
         scale: null,
         latent: null,
-        mins: [...Array(300).keys()].map((i) => -3 * STDS[i]),
-        maxs: [...Array(300).keys()].map((i) => 3 * STDS[i]),
+        EXP: EXP,
+        mins: [...Array(126).keys()].map((i) => MEAN[i] - 3 * STD[i]),
+        maxs: [...Array(126).keys()].map((i) => MEAN[i] + 3 * STD[i]),
     },
     watch: {
         latent: {
             handler(val) {
                 if (!this.loaded) return;
                 const latent = tf.tensor([val.map((e) => e.v)]);
-                const rec = tf.dot(latent, this.components).mul(this.scale).add(this.mean);
+                const rec = tf.dot(latent, this.components).add(SHIFT);
                 const prediction = this.model.predict(rec).reshape([64, 64, 3]);
                 tf.browser.toPixels(prediction, this.$refs.canvas);
             },
@@ -22,24 +23,22 @@ const app = new Vue({
     },
     computed: {
         loaded() {
-            return this.components !== null && this.mean !== null && this.scale !== null;
+            return this.components !== null && this.model !== null;
         },
     },
     methods: {
-        modelLoaded(components, mean, scale, model) {
+        modelLoaded(components, model) {
             this.components = components;
-            this.mean = mean;
-            this.scale = scale;
             this.model = model;
             this.goToMean();
         },
         goToMean() {
-            this.latent = MEANS.map((v) => ({ v }));
+            this.latent = MEAN.map((v) => ({ v }));
         },
         async generateRandom() {
             let latent = [];
-            for (let i = 0; i < 300; i++) {
-                latent.push(tf.randomNormal([1], 0, STDS[i]));
+            for (let i = 0; i < 126; i++) {
+                latent.push(tf.randomNormal([1], MEAN[i], STD[i]));
             }
             latent = tf.stack(latent, (axis = 1));
             this.latent = Array.from(await latent.data()).map((v) => ({ v }));
@@ -50,19 +49,12 @@ const app = new Vue({
 const loadNpy = (url) => new Promise((resolve) => NumpyLoader.ajax(url, resolve));
 
 async function init() {
-    const [components, mean, scale, model] = await Promise.all([
+    const [components, model] = await Promise.all([
         loadNpy("components.npy"),
-        loadNpy("mean.npy"),
-        loadNpy("scale.npy"),
         tf.loadLayersModel("decoder/model.json"),
     ]);
 
-    app.modelLoaded(
-        tf.transpose(tf.tensor(components.data).reshape([300, 300])),
-        tf.tensor(mean.data).reshape([300]),
-        tf.tensor(scale.data).reshape([300]),
-        model
-    );
+    app.modelLoaded(tf.transpose(tf.tensor(components.data).reshape([300, 126])), model);
 }
 
 init();
